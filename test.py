8311b7ce4dec9891b79be41a55b33ff5d8d47379
siddharth Tyagi <sidtyagi05@gmail.com>
2024-02-25 05:09:48 +0530

    utils added

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from us_visa.logger import logging
 from us_visa.exception import USvisaException
 import sys
+
 try:
-    a=1/"10"
+    a = 1 / "10"
 except Exception as e:
-    raise USvisaException (e,sys) from e
+    logging.info(e)
+    raise USvisaException(e, sys) from e
